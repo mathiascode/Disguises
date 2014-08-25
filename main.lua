237dca348c13cf439cdcf5bfd942699feb4b7dc2
tonibm19 <tonibm19@gmail.com>
2014-08-25 14:18:55 +0200

    Initial commit.
    Allows you to disguise and undisguise,

---
+++ main.lua
@@ -0,0 +1,58 @@
+mobid = {}
+timer = {}
+
+function Initialize(Plugin)
+	Plugin:SetName("DisguiseCraft")
+	Plugin:SetVersion(0)
+
+	cPluginManager.AddHook(cPluginManager.HOOK_WORLD_TICK, OnWorldTick)
+    cPluginManager.BindCommand("/d",      "disguisecraft.disguise", HandleDisguiseCommand, "- Disguise as a mob");
+    cPluginManager.BindCommand("/ud",      "disguisecraft.undisguise", HandleUnDisguiseCommand, "- Undisguise");
+
+	LOG("Initialized " .. Plugin:GetName() .. " v." .. Plugin:GetVersion())
+	return true
+end
+
+
+
+
+function OnWorldTick(World, TimeDelta)
+    local MoveMob = function(Entity)
+        local Monster = tolua.cast(Entity,"cMonster")
+        Monster:SetPitch(PlayerID:GetPitch())
+        Monster:SetYaw(PlayerID:GetYaw())
+        Monster:MoveToPosition(PlayerID:GetPosition() + PlayerID:GetLookVector())
+    end
+    local Player = function(Player)
+        PlayerID = Player
+        pos = Player:GetPosition()
+        World:DoWithEntityByID(mobid[Player:GetName()], MoveMob)
+    end
+    World:ForEachPlayer(Player)
+end
+
+function HandleDisguiseCommand(Split, Player)
+    if Split[2] == nil then
+        Player:SendMessageInfo("Usage: /d [mobtype]")
+    else
+        Mob = StringToMobType(Split[2])
+        if Mob == mtInvalidType then
+            Player:SendMessageFailure("Invalid mob type")
+        else
+            Player:SetVisible(false)
+            mobid[Player:GetName()] = Player:GetWorld():SpawnMob(Player:GetPosX(), Player:GetPosY(), Player:GetPosZ(), Mob)
+        end
+    end
+    return true
+end
+
+function HandleUnDisguiseCommand(Split, Player)
+    Player:SetVisible(true)
+    local Delete = function(Entity)
+        Entity:Destroy()
+    end
+    Player:GetWorld():DoWithEntityByID(mobid[Player:GetName()], Delete)
+    mobid[Player:GetName()] = nil
+    return true
+end
+             

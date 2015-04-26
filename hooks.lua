025e6e40f625473103df2b4760f8263c902fac97
tonibm19 <tonibm19@gmail.com>
2015-04-26 13:32:47 +0200

    v0.1 beta
    Fixed a lot of bugs and improved a lot mob movement, now it's posible
    that someone believes you're really a mob.

---
+++ hooks.lua
@@ -0,0 +1,42 @@
+function OnWorldTick(World, TimeDelta)
+    local MoveMob = function(Entity)
+        local Monster = tolua.cast(Entity,"cMonster")
+        Monster:TeleportToCoords(PlayerID:GetPosX(), PlayerID:GetPosY(), PlayerID:GetPosZ())
+        Monster:MoveToPosition(PlayerID:GetPosition() + PlayerID:GetLookVector())
+    end
+    local Player = function(Player)
+        PlayerID = Player
+        local entityId = mobid[Player:GetName()];
+        if entityId ~= nil then
+            World:DoWithEntityByID(mobid[Player:GetName()], MoveMob)
+        end
+    end
+    World:ForEachPlayer(Player)
+end
+             
+             
+function OnTakeDamage(Receiver, TDI)
+    if TDI.Attacker == nil then
+        return false
+    elseif Receiver:IsPlayer() and TDI.Attacker:IsMob() then
+        Player = tolua.cast(Receiver,"cPlayer")
+        Mob = tolua.cast(TDI.Attacker, "cMonster")
+        if Mob:GetUniqueID() == mobid[Player:GetName()] then
+            return true
+        end
+    elseif Receiver:IsMob() and TDI.Attacker:IsPlayer() then
+        Mob = tolua.cast(Receiver, "cMonster")
+        Player = tolua.cast(TDI.Attacker, "cPlayer")    
+        if Mob:GetUniqueID() == mobid[Player:GetName()] then
+            return true
+        end
+    end
+end    
+                 
+function OnPlayerDestroyed(Player)
+    if mobid[Player:GetName()] ~= nil then
+        Player:GetWorld():DoWithEntityByID(mobid[Player:GetName()], cEntity.Destroy)
+        Player:SetVisible(true)
+    end
+end    
+        
--- main.lua
+++ main.lua
@@ -2,10 +2,11 @@ mobid = {}
 
 function Initialize(Plugin)
 	Plugin:SetName("DisguiseCraft")
-	Plugin:SetVersion(0)
+	Plugin:SetVersion(0.1)
 
 	cPluginManager.AddHook(cPluginManager.HOOK_WORLD_TICK, OnWorldTick)
 	cPluginManager:AddHook(cPluginManager.HOOK_TAKE_DAMAGE, OnTakeDamage);	
+	cPluginManager:AddHook(cPluginManager.HOOK_PLAYER_DESTROYED, OnPlayerDestroyed);		
     cPluginManager.BindCommand("/d",      "disguisecraft.disguise", HandleDisguiseCommand, "- Disguise as a mob");
     cPluginManager.BindCommand("/ud",      "disguisecraft.undisguise", HandleUnDisguiseCommand, "- Undisguise");
 
@@ -13,63 +14,16 @@ function Initialize(Plugin)
 	return true
 end
 
-
-
-
-function OnWorldTick(World, TimeDelta)
-    local MoveMob = function(Entity)
-        local Monster = tolua.cast(Entity,"cMonster")
-        Monster:SetPosition(PlayerID:GetPosX(), PlayerID:GetPosY(), PlayerID:GetPosZ())
-        Monster:MoveToPosition(PlayerID:GetPosition() + PlayerID:GetLookVector())
-    end
-    local Player = function(Player)
-        PlayerID = Player
-        local entityId = mobid[Player:GetName()];
-        if entityId ~= nil then
-            World:DoWithEntityByID(mobid[Player:GetName()], MoveMob)
+function OnDisable()
+    local RemoveMob = function(Player)
+        if mobid[Player:GetName()] ~= nil then
+            Player:GetWorld():DoWithEntityByID(mobid[Player:GetName()], cEntity.Destroy)
+            Player:SendMessageWarning(" You've been undisguised because of server restart/reload")
+            Player:SetVisible(true)
         end
-    end
-    World:ForEachPlayer(Player)
-end
-
-function HandleDisguiseCommand(Split, Player)
-    if Split[2] == nil then
-        Player:SendMessageInfo("Usage: /d [mobtype]")
-    else
-        Mob = cMonster:StringToMobType(Split[2])
-        if Mob == mtInvalidType then
-            Player:SendMessageFailure("Invalid mob type")
-        else
-            Player:SetVisible(false)
-            mobid[Player:GetName()] = Player:GetWorld():SpawnMob(Player:GetPosX(), Player:GetPosY(), Player:GetPosZ(), Mob)
-        end
-    end
-    return true
-end
-
-function HandleUnDisguiseCommand(Split, Player)
-    Player:SetVisible(true)
-    Player:GetWorld():DoWithEntityByID(mobid[Player:GetName()], cEntity.Destroy)
-    mobid[Player:GetName()] = nil
-    return true
-end
-             
-             
-function OnTakeDamage(Receiver, TDI)
-    if TDI.Attacker == nil then
-        return false
-    elseif Receiver:IsPlayer() and TDI.Attacker:IsMob() then
-        Player = tolua.cast(Receiver,"cPlayer")
-        Mob = tolua.cast(TDI.Attacker, "cMonster")
-        if Mob:GetUniqueID() == mobid[Player:GetName()] then
-            return true
-        end
-    elseif Receiver:IsMob() and TDI.Attacker:IsPlayer() then
-        Mob = tolua.cast(Receiver, "cMonster")
-        Player = tolua.cast(TDI.Attacker, "cPlayer")    
-        if Mob:GetUniqueID() == mobid[Player:GetName()] then
-            return true
-        end
-    end
+    end    
+    cRoot:Get():ForEachPlayer(RemoveMob)
 end    
-                 
+
+
+        

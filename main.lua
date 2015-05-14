d9ac329bc12646808eb3a9e5359c141d86bf6ed5
Mathias <mathias-github@users.noreply.github.com>
2015-05-14 12:58:34 +0300

    Spaces to tabs

--- main.lua
+++ main.lua
@@ -7,22 +7,22 @@ function Initialize(Plugin)
 	cPluginManager.AddHook(cPluginManager.HOOK_WORLD_TICK, OnWorldTick)
 	cPluginManager:AddHook(cPluginManager.HOOK_TAKE_DAMAGE, OnTakeDamage);	
 	cPluginManager:AddHook(cPluginManager.HOOK_PLAYER_DESTROYED, OnPlayerDestroyed);		
-    cPluginManager.BindCommand("/d",      "disguisecraft.disguise", HandleDisguiseCommand, "- Disguise as a mob");
-    cPluginManager.BindCommand("/ud",      "disguisecraft.undisguise", HandleUnDisguiseCommand, "- Undisguise");
+	cPluginManager.BindCommand("/d",      "disguisecraft.disguise", HandleDisguiseCommand, "- Disguise as a mob");
+	cPluginManager.BindCommand("/ud",      "disguisecraft.undisguise", HandleUnDisguiseCommand, "- Undisguise");
 
 	LOG("Initialized " .. Plugin:GetName() .. " v." .. Plugin:GetVersion())
 	return true
 end
 
 function OnDisable()
-    local RemoveMob = function(Player)
-        if mobid[Player:GetName()] ~= nil then
-            Player:GetWorld():DoWithEntityByID(mobid[Player:GetName()], cEntity.Destroy)
-            Player:SendMessageWarning(" You've been undisguised because of server restart/reload")
-            Player:SetVisible(true)
-        end
-    end    
-    cRoot:Get():ForEachPlayer(RemoveMob)
+	local RemoveMob = function(Player)
+		if mobid[Player:GetName()] ~= nil then
+			Player:GetWorld():DoWithEntityByID(mobid[Player:GetName()], cEntity.Destroy)
+			Player:SendMessageWarning(" You've been undisguised because of server restart/reload")
+			Player:SetVisible(true)
+		end
+	end    
+	cRoot:Get():ForEachPlayer(RemoveMob)
 end    
 
 
